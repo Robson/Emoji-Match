0927803f39d474b47cbf8beb83e4c15fab5e318e
Robson <relative@gmail.com>
2021-07-09 18:21:13 +0100

    Added the Options screen

--- page.js
+++ page.js
@@ -13,12 +13,14 @@
 		right side is highlighted
 	
 	[STATE-2]
-		"START" button is removed
+	
+	[STATE-3]
+		"NEXT" button is removed
 		right side is highlighted
 		row of emojis appear on the right
 		user is prompted to choose a similar emoji from the right
 	
-	[STATE-3]
+	[STATE-4]
 		non-selected clue emojis are faded
 		left side is highlighted
 */
@@ -26,10 +28,11 @@
 var emojisChosen = [], emojisClues = [];
 
 var state = 0;
-var potentialAnswers = 6;
+var potentialAnswerCount = 6;
+var clueEmojiCount = 5;
 var answer = -1;
 var guessed = 0;
-var selectedCategory = 'Object';
+var selectedCategory = 'Nature';
 
 function selectLeftSide() {	
 	d3.select('#answers').style('background', '#fdb');	
@@ -47,22 +50,25 @@ function startGame() { // state 0
 	state = 0;
 	guessed = 0;
 	selectLeftSide();
-	var emojisRandom = getCategoryEmojisOnly();
-	emojisChosen = emojisRandom.slice(0, potentialAnswers);
-	emojisClues = emojisRandom.slice(potentialAnswers);
+	var emojisRandom = getCategoryEmojisOnly(selectedCategory);
+	emojisChosen = emojisRandom.slice(0, potentialAnswerCount);
+	emojisClues = emojisRandom.slice(potentialAnswerCount);
 	createPotentialGuessCards();
-	d3.select('#help').html("<em>Guessers </em> look away while <em>Player 1</em> selects a target emoji")
+	d3.select('#help').html("<em>Guessers </em> look away, while <em>Player 1</em> selects a target emoji")
+	d3.selectAll('.guess_container').remove();
+	d3.select('#different_targets').style('display', 'inline-block');
+	d3.select('#restart').style('display', 'none');
 }
 
-function getCategoryEmojisOnly() {
-	var arr = [...new Set(emojis.filter(x => x[1].includes(selectedCategory)).map(x => x[0]))];
+function getCategoryEmojisOnly(cat) {
+	var arr = [...new Set(emojis.filter(x => x[1].includes(cat)).map(x => x[0]))];
 	return shuffle(arr);
 }
 
 function newTargetEmojiSet() {
-	var emojisRandom = getCategoryEmojisOnly();
-	emojisChosen = emojisRandom.slice(0, potentialAnswers);
-	emojisClues = emojisRandom.slice(potentialAnswers);
+	var emojisRandom = getCategoryEmojisOnly(selectedCategory);
+	emojisChosen = emojisRandom.slice(0, potentialAnswerCount);
+	emojisClues = emojisRandom.slice(potentialAnswerCount);
 	createPotentialGuessCards();
 }
 
@@ -116,12 +122,12 @@ function clickedGuess(element) {
 			d3.selectAll('.card')
 				.style('cursor', 'not-allowed')
 				.on('mousedown', null);
-			d3.select('#start').style('display', 'inline-block');
-			d3.select('#help').html('<em>Player 1:</em> remember your emoji and click Start!');
-			d3.select('#different_targets').style('display', 'none')
+			d3.select('#next').style('display', 'inline-block');
+			d3.select('#help').html('<em>Player 1</em>: remember your emoji and click Next');
+			d3.select('#different_targets').style('display', 'none');
 			state = 1;
 			break;
-		case 3:
+		case 4:
 			d3.selectAll('.card')
 				.style('cursor', 'not-allowed')
 				.classed('selecting', false)
@@ -131,29 +137,35 @@ function clickedGuess(element) {
 				d3.selectAll('.card')
 					.style('cursor', 'not-allowed');
 				d3.select('#help').html("Whoops! Accidentally clicked the target emoji! Game over");
+				d3.select('#restart').style('display', 'inline-block');
 			} else {
 				guessed++;
 				chosen.attr('class', 'card disabled');
 				if (guessed + 1 == emojisChosen.length) {
 					d3.select('#help').html("Congratulations! The target emoji has been found!");
 					d3.select('#card' + answer).attr('class', 'card selected_answer');
+					d3.select('#restart').style('display', 'inline-block');
 				} else {
-					selectRightSide();
-					d3.select('#help').html("Good guess! <em>Player 1</em> select an emoji on the right, which is most similar to your target emoji");
-					state = 2;
-					makeGuessEmojis();
+					clickStartGame();
 				}
 			}
 			break;
 	}
 }
 
-function clickStart() {
-	d3.select('#start').style('display', 'none');
+function clickCommitTargetEmoji() {
+	d3.select('#next').style('display', 'none');
+	d3.select('#start').style('display', 'inline-block');
 	d3.select('.selected_answer').attr('class', 'card');
-	d3.select('#help').html("<em>Guessers</em> can look now. <em>Player 1</em> select an emoji on the right, which is most similar to your target emoji");
-	makeGuessEmojis();
+	d3.select('#help').html("<em>Player 1</em>: tell the <em>Guessers</em> that they can look, then press Start");	
 	state = 2;
+}
+
+function clickStartGame() {
+	d3.select('#start').style('display', 'none');
+	d3.select('#help').html("<em>Player 1</em>: select an emoji on the right, which is most similar to your target emoji");
+	makeGuessEmojis();
+	state = 3;
 }
 
 function makeGuessEmojis() {
@@ -161,7 +173,7 @@ function makeGuessEmojis() {
 	gc.lower();
 	d3.selectAll('.active').classed('active', false);
 	gc.attr('class', 'guess_container active');
-	for (var i = 0; i < 5; i++) {
+	for (var i = 0; i < clueEmojiCount; i++) {
 		var css = "rotate([deg]deg) translate([left]px, [top]px)"
 			.replace("[deg]", random(-3, 3))
 			.replace("[left]", random(-1, 1))
@@ -186,12 +198,12 @@ function clickedClueEmoji(element) {
 	d3.selectAll('.minicard')
 		.on('mousedown', null);			
 	selectLeftSide();
-	state = 3;	
+	state = 4;	
 	guessEmoji();
 }
 
 function guessEmoji() {
-	d3.select('#help').html('<em>Guessers</em> select an emoji on the left to discard');
+	d3.select('#help').html("<em>Guessers</em>: select an emoji on the left that you don't think is the target emoji");
 	d3
 		.selectAll('.card')
 		.style('cursor', 'pointer')
@@ -202,6 +214,55 @@ function guessEmoji() {
 		.style('cursor', 'not-allowed')
 		.on('mousedown', null)		
 		.classed('selecting_remove', false);
+}
+
+/***** OPTIONS ******/
+
+function showOptions() {
+	d3.select('#category').property('value', selectedCategory);
+	d3.select('#target_emoji_count').property('value', potentialAnswerCount);
+	d3.select('#clue_emoji_count').property('value', clueEmojiCount);
+	showCategoryPreview();	
+	d3.select('#game').style('opacity', 0.5);
+	d3.select('#cog').style('opacity', 0.5);
+	d3.select('#game').style('filter', 'grayscale(100)');
+	d3.select('#cog').style('filter', 'grayscale(100)');	
+	d3.select('#cover').style('display', 'block');
+	d3.select('#options').style('display', 'block');	
+	d3.select('#cog').style('display', 'none');
+}
+
+function showCategoryPreview() {
+	var box = d3.select('#category_preview');
+	box.selectAll('*').remove();
+	var listCategory = document.getElementById('category').value || 'Object';
+	var previewEmojis = getCategoryEmojisOnly(listCategory);
+	previewEmojis = previewEmojis.slice(0, 5);
+	box.text(previewEmojis.join(' '));
+}
+
+function optionsCancel() {
+	d3.select('#game').style('opacity', 1);
+	d3.select('#cog').style('opacity', 1);
+	d3.select('#game').style('filter', 'none');
+	d3.select('#cog').style('filter', 'none');	
+	d3.select('#cover').style('display', 'none');
+	d3.select('#options').style('display', 'none');	
+	d3.select('#cog').style('display', 'block');	
+}
+
+function optionsApply() {
+	d3.select('#game').style('opacity', 1);
+	d3.select('#cog').style('opacity', 1);
+	d3.select('#game').style('filter', 'none');
+	d3.select('#cog').style('filter', 'none');	
+	d3.select('#cover').style('display', 'none');
+	d3.select('#options').style('display', 'none');	
+	d3.select('#cog').style('display', 'block');
+	selectedCategory = document.getElementById('category').value;
+	potentialAnswerCount = document.getElementById('target_emoji_count').value;
+	clueEmojiCount = document.getElementById('clue_emoji_count').value;
+	startGame();
 }
 
 /***** GENERIC ******/
